8d378782644f60ecca527aa4f4e89ea381815694
frederic.waldmann@fhnw.ch <frederic.waldmann@fhnw.ch>
2022-06-29 15:02:55 +0200

    example-ex130.0-jit.py extend from branch example_jit. jit multi set recursions added. Not Implemented is jit Multi Output in combination with multi Set i.e. y of shape (K, M, S). Needs further investigation.

--- examples/13-backend/example-ex130.0-jit.py
+++ examples/13-backend/example-ex130.0-jit.py
@@ -8,8 +8,9 @@ Comparison between jit backend and python
 import timeit
 
 import lmlib as lm
-from lmlib.utils import load_data
+from lmlib.utils import load_data, load_data_mc
 
+print('Single channel')
 y = load_data('EECG_BASELINE_1CH_10S_FS2400HZ.csv')
 alssm = lm.AlssmPoly(3)
 seg_l = lm.Segment(-50, -1, lm.FW, 1000)
@@ -17,9 +18,10 @@ seg_r = lm.Segment(0, 49, lm.BW, 1000)
 cost = lm.CompositeCost([alssm], [seg_l, seg_r], F=[[1, 1]])
 
 rls_py = lm.RLSAlssm(cost)
+xs_py = rls_py.filter_minimize_x(y)
 rls_jit = lm.RLSAlssm(cost)
 rls_jit.set_backend('jit')
-rls_jit.filter_minimize_x(y) # create just in time compilation
+xs_jit = rls_jit.filter_minimize_x(y) # create just in time compilation
 
 proc_time_py = timeit.timeit('rls_py.filter_minimize_x(y)', globals=globals(), number=10)
 proc_time_jit = timeit.timeit('rls_jit.filter_minimize_x(y)', globals=globals(), number=10)
@@ -27,3 +29,25 @@ proc_time_jit = timeit.timeit('rls_jit.filter_minimize_x(y)', globals=globals(),
 print(f'python backend: {proc_time_py:.3} sec')
 print(f'jit backend: {proc_time_jit:.3} sec')
 print(f'speed up factor of jit: {proc_time_py/proc_time_jit}')
+
+
+print('Multi channel Set')
+y = load_data_mc('EECG_FILT_9CH_10S_FS2400HZ.csv')
+alssm = lm.AlssmPoly(3)
+seg_l = lm.Segment(-50, -1, lm.FW, 1000)
+seg_r = lm.Segment(0, 49, lm.BW, 1000)
+cost = lm.CompositeCost([alssm], [seg_l, seg_r], F=[[1, 1]])
+
+rls_py = lm.RLSAlssmSet(cost)
+xs_py = rls_py.filter_minimize_x(y)
+rls_jit = lm.RLSAlssmSet(cost)
+rls_jit.set_backend('jit')
+xs_jit = rls_jit.filter_minimize_x(y) # create just in time compilation
+
+proc_time_py = timeit.timeit('rls_py.filter_minimize_x(y)', globals=globals(), number=10)
+proc_time_jit = timeit.timeit('rls_jit.filter_minimize_x(y)', globals=globals(), number=10)
+
+print(f'python backend: {proc_time_py:.3} sec')
+print(f'jit backend: {proc_time_jit:.3} sec')
+print(f'speed up factor of jit: {proc_time_py/proc_time_jit}')
+
