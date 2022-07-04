62e7cf9fd8e1ac28f990c1074a499b9f9b27e449
frederic.waldmann@fhnw.ch <frederic.waldmann@fhnw.ch>
2022-07-04 16:32:40 +0200

    moved example-ex130.0-jit.py to coding and added global jit

--- coding/13-backend/example-ex130.0-jit.py
+++ coding/13-backend/example-ex130.0-jit.py
@@ -2,13 +2,16 @@
 JIT VS Python Backend [ex130.0]
 ===============================
 
-Comparison between jit backend and python
+Comparison between jit (just in time) and python backend
 
 """
 import timeit
 
 import lmlib as lm
 from lmlib.utils import load_data, load_data_mc
+
+# setting all recursions to JIT backend
+lm.set_backend('jit')
 
 print('Single channel')
 y = load_data('EECG_BASELINE_1CH_10S_FS2400HZ.csv')
@@ -18,9 +21,10 @@ seg_r = lm.Segment(0, 49, lm.BW, 1000)
 cost = lm.CompositeCost([alssm], [seg_l, seg_r], F=[[1, 1]])
 
 rls_py = lm.RLSAlssm(cost)
+rls_py.set_backend('py') # set individual RLSAlssm back to python backend
 xs_py = rls_py.filter_minimize_x(y)
+
 rls_jit = lm.RLSAlssm(cost)
-rls_jit.set_backend('jit')
 xs_jit = rls_jit.filter_minimize_x(y) # create just in time compilation
 
 proc_time_py = timeit.timeit('rls_py.filter_minimize_x(y)', globals=globals(), number=10)
@@ -39,9 +43,10 @@ seg_r = lm.Segment(0, 49, lm.BW, 1000)
 cost = lm.CompositeCost([alssm], [seg_l, seg_r], F=[[1, 1]])
 
 rls_py = lm.RLSAlssmSet(cost)
+rls_py.set_backend('py')
 xs_py = rls_py.filter_minimize_x(y)
+
 rls_jit = lm.RLSAlssmSet(cost)
-rls_jit.set_backend('jit')
 xs_jit = rls_jit.filter_minimize_x(y) # create just in time compilation
 
 proc_time_py = timeit.timeit('rls_py.filter_minimize_x(y)', globals=globals(), number=10)
