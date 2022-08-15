e750b2797d3a197cb577dfdadbb8dcdf34625ad9
fwaldmann <106607463+fwaldmann@users.noreply.github.com>
2022-08-15 14:00:08 +0200

    Update setup.py
    changed version  to 2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lmlib',
-    version='2.0.2',
+    version='2.1.0',
     description='A Model-Based Signal Processing Library Working With Windowed Linear State-Space and Polynomial Signal Models.',
     author='Reto Wildhaber, Frédéric Waldmann, Luca Fleischmann, Christof Baeriswyl',
     author_email='reto.wildhaber@fhnw.ch, frederic.waldmann@fhnw.ch',
