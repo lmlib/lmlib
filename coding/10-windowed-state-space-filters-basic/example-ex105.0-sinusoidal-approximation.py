b45f609988c1a80b079ac20d9be28694f7eed742
retowildhaber <106626060+retowildhaber@users.noreply.github.com>
2022-11-02 10:20:30 +0100

    Update example-ex105.0-sinusoidal-approximation.py
    orphan line removed from example

--- coding/10-windowed-state-space-filters-basic/example-ex105.0-sinusoidal-approximation.py
+++ coding/10-windowed-state-space-filters-basic/example-ex105.0-sinusoidal-approximation.py
@@ -3,7 +3,6 @@ Local Signal Approximation and Trajectories [ex105.0]
 =====================================================
 
 Local Signal Approximation and Trajectories using :class:`~lmlib.statespace.model.PolyAlssm`.
-
 """
 import matplotlib.pyplot as plt
 import lmlib as lm
@@ -13,14 +12,10 @@ from lmlib.utils.generator import gen_wgn, gen_rect
 K = 2100
 y = gen_rect(K, 570, 200) + gen_wgn(K, 0.01)
 
-pi = 3.1415926535
-
 cost = lm.CostSegment(lm.AlssmPoly(poly_degree=4),
                       lm.Segment(0, 220, lm.BW, 300))
 cost_wide = lm.CostSegment(lm.AlssmPoly(poly_degree=4),
                       lm.Segment(0-20, 220+20, lm.BW, 300))
-
-
 
 rls = lm.RLSAlssmSteadyState(cost) # Using steady state leads to faster computation and increased nummerical stability
 xs = rls.filter_minimize_x(y)
@@ -28,7 +23,6 @@ xs = rls.filter_minimize_x(y)
 K_refs = [500, 1130, 1800]
 trajs = lm.map_trajectories(cost.trajectories(xs[K_refs], thd=0.01), K_refs, K, merge_ks=True, merge_seg=True)
 trajs_wide = lm.map_trajectories(cost_wide.trajectories(xs[K_refs], thd=0.01), K_refs, K, merge_ks=True, merge_seg=True)
-
 
 plt.title('Local Signal Approximation: RLSAlssm.filter_minimize_x(y)')
 plt.plot(y, lw=0.3, c='k', label='y')
