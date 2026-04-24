232292b35a5bc384ac8a5fae7e14ff020a3b8a08
Pedro Devogelaere <pedro.devogelaere@kuleuven.be>
2026-04-24 14:56:03 +0200

    Example: example 131.0

--- coding/13-backend/example-ex131.0-compare.py
+++ coding/13-backend/example-ex131.0-compare.py
@@ -2,7 +2,7 @@
 Benchmarking of Stace-Space vs. Transfer-Function Backend [ex131.0]
 ===================================================================
 
-This example demonstrates the usage of transfer-function (tf) backend in RLSAlssm* classes.
+This example demonstrates the usage of transfer-function (tf) backend in RLSAlssm* classes. Here we use 6 different channels instead of one.
 """
 import timeit
 import numpy as np
